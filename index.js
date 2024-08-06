2195e1d141109e9627a5dc58f5fcc020a7b7de04
Samprity Mukherjee <mukherjeesamprity@gmail.com>
2024-08-06 21:07:37 +0530

    fix : API endpoint

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
   res.status(200).send('Hello World');
 });
 
